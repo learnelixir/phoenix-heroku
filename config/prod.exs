7ce0b0f26960283e8f1079c89ae732ba4f3cacc3
Learn Elixir <learnelixir@gmail.com>
2014-10-26 23:26:26 +0800

    initial commit

---
+++ config/prod.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# ## SSL Support
+#
+# To get SSL working, you will need to set:
+#
+#     https: [port: 443,
+#             keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+#             certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
+#
+# Where those two env variables point to a file on
+# disk for the key and cert.
+
+config :phoenix, PhoenixHeroku.Router,
+  url: [host: "example.com"],
+  http: [port: System.get_env("PORT")],
+  secret_key_base: "3f0JoEl+Ijbte28aV0WzusJg/RxkAiytWTYmXQid38P1FM0/JmgH2OFcevP6ztGTmYPjEt2oGMP8qHkLMwyE7w=="
+
+config :logger, :console,
+  level: :info
